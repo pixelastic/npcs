07a9d0681cc6eefac72bc3148f4845b2c2e3b66d
Pixelastic <tim@pixelastic.com>
2020-05-25 15:21:15 +0200

    feat(main): Main page

--- src/_data/avatars.js
+++ src/_data/avatars.js
@@ -2,9 +2,12 @@ const glob = require('firost/lib/glob');
 const _ = require('golgoth/lib/lodash');
 const path = require('path');
 
-module.export = async function() {
-  const list = await glob('src/assets/portraits/*.jpg');
-  return _.map(list, item => {
-    return path.basename(item, '.jpg');
-  });
+module.exports = async function() {
+  const list = await glob(path.resolve('src/assets/portrait/*.jpg'));
+  return _.chain(list)
+    .map(item => {
+      return path.basename(item, '.jpg');
+    })
+    .drop(2)
+    .value();
 };
