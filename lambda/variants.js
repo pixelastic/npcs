dc5457d1889175cb917bf8bf158ee007a3b51dd9
Pixelastic <tim@pixelastic.com>
2020-06-05 16:07:22 +0200

    feat(add): Allow generating variants

--- lambda/variants.js
+++ lambda/variants.js
@@ -1,33 +1,25 @@
 const secrets = require('./lib/secrets.js');
 const responseHelper = require('callirhoe/lib/responseHelper');
 const sentry = require('callirhoe/lib/sentry');
-const parseDataURL = require('data-urls');
 const got = require('golgoth/lib/got');
 const FormData = require('form-data');
-const path = require('path');
 sentry.init(secrets.SENTRY_DSN);
 
 /**
- * Take an image as input and return the portrait version as output
+ * Take a portraitId from portraitai.com and return a variant as base64
  * @param {object} rawRequest Request object
  * @returns {object} Response object
  */
 async function handler(rawRequest) {
   const response = new responseHelper();
 
-  // Getting the picture as a base64 encoded file
-  const data = JSON.parse(rawRequest.body);
-  const source = data.source;
-
-  // Converting it to a buffer
-  const parsedDataURL = parseDataURL(source);
-  const content = parsedDataURL.body;
+  const { portraitId } = JSON.parse(rawRequest.body);
 
   // Simulating a form to upload
   const form = new FormData();
   form.append('id', '11111-SHb6');
   form.append('wm', '3');
-  form.append('image', content, 'filename.jpg');
+  form.append('code', `${portraitId}-_port`);
 
   // Submitting the form to portraitai
   const postUrl = 'https://portraitplus.facefun.ai/Port/MakePort';
@@ -42,17 +34,12 @@ async function handler(rawRequest) {
     return response.output('nope');
   }
 
-  console.info({ portraitUrl });
-
   // Get the portrait as base64
   const portraitBuffer = await got(portraitUrl).buffer();
   const portraitAsBase64 = portraitBuffer.toString('base64');
   const portraitAsDataURL = `data:image/png;base64,${portraitAsBase64}`;
 
-  const portraitId = path.basename(portraitUrl).replace('-_port.jpg', '');
-
   const responseData = {
-    portraitId,
     dataURL: portraitAsDataURL,
   };
 
--- src/_scripts/add.js
+++ src/_scripts/add.js
@@ -1,119 +1,169 @@
 const ky = require('ky').default;
+const pMap = require('p-map');
+// TODO:
+// Catch errors when something break in the lambda, ask to try with
+// another image
+// Once one is selected, allow for submitting
+// Allow sending a new file
+// Clear all previews when selecting a new file
 module.exports = {
   data: {
     source: null,
   },
-  apiUrl(folder) {
-    const baseApiUrl = window.CONFIG.apiUrl;
-    return `${baseApiUrl}${folder}`;
-  },
-  isEnabled() {
-    return this.byId('addPage');
-  },
-  byId(selector) {
-    return document.getElementById(selector);
-  },
-  getPictureUpload() {
-    return this.byId('pictureUpload');
-  },
-  getPicturePreview() {
-    return this.byId('picturePreview');
-  },
-  getPortraitPreview(index) {
-    return this.byId(`portraitPreview${index}`);
-  },
-  async getFileAs(file, as) {
-    const reader = new FileReader();
-    reader[`readAs${as}`](file);
-    return new Promise(resolve => {
-      reader.onload = function() {
-        resolve(this.result);
-      };
-    });
-  },
-  setStep(stepName) {
-    this.byId('addForm').setAttribute('data-step', stepName);
-  },
-  async displayPreview(file) {
-    const dataURL = await this.getFileAs(file, 'DataURL');
-    const picturePreview = this.getPicturePreview();
-    return new Promise(resolve => {
-      picturePreview.onload = resolve;
-      picturePreview.src = dataURL;
-    });
-  },
-  async getSourceDataURL(file) {
-    const dataURL = await this.getFileAs(file, 'DataURL');
-
-    const tmpImage = this.byId('tmpImage');
-    const tmpCanvas = this.byId('tmpCanvas');
-    const finalDimension = 512;
-
-    return new Promise(resolve => {
-      tmpImage.onload = () => {
-        const imageWidth = tmpImage.offsetWidth;
-        const imageHeight = tmpImage.offsetHeight;
-        tmpCanvas.width = finalDimension;
-        tmpCanvas.height = finalDimension;
-        tmpCanvas
-          .getContext('2d')
-          .drawImage(
-            tmpImage,
-            0,
-            0,
-            imageWidth,
-            imageHeight,
-            0,
-            0,
-            finalDimension,
-            finalDimension
-          );
-        const canvasDataURL = tmpCanvas.toDataURL('image/png');
-
-        resolve(canvasDataURL);
-      };
-      tmpImage.src = dataURL;
-    });
-  },
-  handleInputSelection() {
-    const pictureUpload = this.getPictureUpload();
+  /**
+   * Init the form interactivity
+   **/
+  init() {
+    const pictureUpload = this.byId('pictureUpload');
     pictureUpload.addEventListener(
       'change',
       async () => {
         this.setStep('sourceSelected');
 
         const selectedFile = pictureUpload.files[0];
-        await this.displayPreview(selectedFile);
-        const sourceDataURL = await this.getSourceDataURL(selectedFile);
+        const dataURL = await this.getSelectedFileDataURL(selectedFile);
+
+        // We display the preview directly from the selected file
+        const picturePreview = this.byId('picturePreview');
+        await this.loadImage(picturePreview, dataURL);
+
+        // We get the source dataURL by writing it to a canvas offscreen
+        const sourceDataURL = await this.convertAndResizeToPNG(dataURL, 512);
 
         // Save the file
         this.data.source = sourceDataURL;
 
         // Ask the API for portrait
-        this.handlePortraitGeneration(sourceDataURL);
+        this.generatePortraits(sourceDataURL);
       },
       false
     );
   },
-  async handlePortraitGeneration(base64) {
+  /**
+   * Returns true if we're on the add page
+   * @returns {boolean} True if we're on the add page
+   **/
+  isEnabled() {
+    return this.byId('addPage');
+  },
+  /**
+   * Wrapper around document.getElementById
+   * @param {string} id Id selector
+   * @returns {object} HTML object
+   **/
+  byId(id) {
+    return document.getElementById(id);
+  },
+  /**
+   * Change the current step of the form.
+   * It internally changes an HTML attibute and toggling element is handled in
+   * CSS
+   * @param {string} stepName Name of the current step
+   **/
+  setStep(stepName) {
+    this.byId('addForm').setAttribute('data-step', stepName);
+  },
+  /**
+   * Load an image and wait until it's loaded
+   * @param {object} image Image HTML tag
+   * @param {string} src Src value of the image
+   * @returns {object} The image HTML tag
+   **/
+  async loadImage(image, src) {
+    return new Promise(resolve => {
+      image.onload = () => {
+        resolve(image);
+      };
+      image.src = src;
+    });
+  },
+  /**
+   * Convert a dataURL into a resized png dataURL
+   * @param {string} inputDataURL Source dataURl
+   * @param {number} newDimension Length of one new side
+   * @returns {string} Final dataURL
+   **/
+  async convertAndResizeToPNG(inputDataURL, newDimension) {
+    const tmpImage = this.byId('tmpImage');
+    const tmpCanvas = this.byId('tmpCanvas');
+
+    return new Promise(resolve => {
+      tmpImage.onload = () => {
+        tmpCanvas.width = newDimension;
+        tmpCanvas.height = newDimension;
+        tmpCanvas
+          .getContext('2d')
+          .drawImage(
+            tmpImage,
+            0,
+            0,
+            tmpImage.offsetWidth,
+            tmpImage.offsetHeight,
+            0,
+            0,
+            newDimension,
+            newDimension
+          );
+        const canvasDataURL = tmpCanvas.toDataURL('image/png');
+
+        resolve(canvasDataURL);
+      };
+      tmpImage.src = inputDataURL;
+    });
+  },
+  /**
+   * Returns the dataURL representing a file selected by the upload input
+   * @param {object} selectedFile File selected in the file input
+   * @returns {string} dataURL of the file
+   **/
+  async getSelectedFileDataURL(selectedFile) {
+    const reader = new FileReader();
+    return new Promise(resolve => {
+      reader.readAsDataURL(selectedFile);
+      reader.onload = function() {
+        resolve(this.result);
+      };
+    });
+  },
+
+  /**
+   * Return the full API url to a given endpoint
+   * @param {string} entrypoint API entrypoint
+   * @returns {string} Full API endpoint
+   **/
+  apiUrl(entrypoint) {
+    const baseApiUrl = window.CONFIG.apiUrl;
+    return `${baseApiUrl}${entrypoint}`;
+  },
+
+  /**
+   * Generate portraits from the source and display them
+   * @param {string} sourceDataURL dataURL of the source image
+   **/
+  async generatePortraits(sourceDataURL) {
     const apiUrl = this.apiUrl('portraitify');
     const data = {
-      source: base64,
+      source: sourceDataURL,
     };
     const response = await ky.post(apiUrl, { json: data }).json();
 
-    const portraitPreview = this.getPortraitPreview(1);
-    portraitPreview.src = response.dataURL;
+    // Display the first preview
+    const mainPortraitPreview = this.byId('portraitPreview1');
+    mainPortraitPreview.src = response.dataURL;
 
-    // TODO:
-    // Catch errors when something break in the lambda, ask to try with
-    // another image
-    // Once one is displayed, start loading the others with /alternatives
-    // Once one is selected, allow for submitting
-    // Clear all previews when selecting a new file
-    // Submit the real and portrait to the function
-  },
-  init() {
-    this.handleInputSelection();
+    // Display variants
+    const portraitId = response.portraitId;
+    const variantUrl = this.apiUrl('variants');
+    const variantData = {
+      portraitId,
+    };
+    await pMap([2, 3, 4], async index => {
+      const variantResponse = await ky
+        .post(variantUrl, { json: variantData })
+        .json();
+      const portraitPreview = this.byId(`portraitPreview${index}`);
+      portraitPreview.src = variantResponse.dataURL;
+    });
+    console.info('All portraits downloaded');
   },
 };
